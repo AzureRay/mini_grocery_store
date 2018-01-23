5fdf3bdd684129f34aaba334972a74cc777f6564
AzureRay <roseluo1993@163.com>
2018-01-23 22:23:00 +0800

    update

--- pages/articles/article-detail/article-detail.js
+++ pages/articles/article-detail/article-detail.js
@@ -1,82 +1,65 @@
-var postsData = require('../../data/articles-data.js')
-// var postsData = require('/posts/data/articles-data.js')
-
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    //小程序总是会读取data对象来做数据绑定，这个动作我们称为动作A
-    // 而这个动作A的执行，是在onLoad函数执行之后发生的
-    imgUrls: [
-      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
-    ]
+    
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    // this.data.postList = postsData.postList
-    this.setData({
-      postList: postsData.postList
-    });
-
+    
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+    
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
-  },
-  enterArticleDetail:function(event){
-    var postId = event.currentTarget.dataset.postid;
-    console.log(postId);
+    
   }
 })
